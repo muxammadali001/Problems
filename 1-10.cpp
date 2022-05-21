20bac8e9b9a11e0e72fd648b37f438a8b1edd697
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-21 13:26:53 +0500

    c++ 1 - 10

---
+++ 1-10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout <<"222222"<< endl;
+  cout <<"2    2"<< endl;
+  cout <<"2    2"<< endl;
+  cout <<"222222"<< endl;
+ return 0;
+ }
