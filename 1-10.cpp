06876f278f90e2874ec761e9f799a3578269bd8f
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-21 16:55:59 +0500

    second comment

--- 1-10.cpp
+++ 1-10.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
-int main()
+int main1()
 {
   cout <<"222222"<< endl;
   cout <<"2    2"<< endl;
   cout <<"2    2"<< endl;
   cout <<"222222"<< endl;
  return 0;
+ }
+ int main()
+ {
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "" << endl;
+     return 0;
  }
