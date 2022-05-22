f2aea2ddd45c5d79638cf34570e268f76675a4db
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-22 14:42:36 +0500

    c++

--- 1-10.cpp
+++ 1-10.cpp
@@ -18,11 +18,29 @@ int main1()
   
   return 0;
  }
- int main()
+ int main3()
 {
   cout <<"\\    /\\"<< endl;
   cout <<" )  ( ')"<< endl;
   cout <<"(  /  )"<< endl;
   cout << " \\(__)|"<< endl;
+  return 0;
+}
+int main4()
+{
+  
+  cout << "|\\_/|" << endl;
+  cout << "|qp| /}" << endl;
+  cout << "( 0 )\"\"\"\\" << endl;
+  cout << "|\"^\"` |" << endl;
+  cout << "||_/=\\\\__|" << endl;  
+ return 0;
+}
+int main()
+{
+  int oy, kun;
+  cin >> oy >> kun;
+  
+  cout << "Birthday is " << setw(2) << setfill('0') << oy << "-" << setw(2) << setfill('0') << kun << "." << endl;
   return 0;
 }
